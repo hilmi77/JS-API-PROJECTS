31dad4f88105a7fd48ebf01dc3f2d6d1c09422cc
Hilmi KALE <120317069+hilmi77@users.noreply.github.com>
2023-06-22 17:56:23 +0300

    themecolor added

--- Pokemon Card Generator/app.js
+++ Pokemon Card Generator/app.js
@@ -1,3 +1,21 @@
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
 const url = " https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
 const btn = document.getElementById("btn");
@@ -27,6 +45,9 @@ let generateCard = (data) => {
   const statDefense = data.stats[2].base_stat;
   const statSpeed = data.stats[5].base_stat;
 
+  // set themecolor based on pokemon type
+  const themeColor = typeColor[data.types[0].type.name];
+
   card.innerHTML = `  <p class="hp">
 <span>HP</span>
 ${hp}
@@ -37,8 +58,6 @@ src="${imgSrc}"
 />
 <h2 class="poke-name">${pokeName}</h2>
 <div class="types">
-<span>type 1</span>
-<span>type 2</span>
 </div>
 <div class="stats">
 <div>
@@ -54,7 +73,14 @@ src="${imgSrc}"
   <p>Speed</p>
 </div>
 </div>`;
+  appendTypes(data.types);
 };
-
+let appendTypes = (types) => {
+  types.forEach((item) => {
+    let span = document.createElement("SPAN");
+    span.textContent = item.type.name;
+    document.querySelector(".types").appendChild(span);
+  });
+};
 btn.addEventListener("click", getPokeData);
 window.addEventListener("load", getPokeData);
